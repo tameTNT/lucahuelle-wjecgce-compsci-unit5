b6159450025ebcf8a79fbac179299c1e0aa592f3
tameTNT <30503695+tameTNT@users.noreply.github.com>
2021-01-10 20:17:47 +0000

    ✨Add password hashing and verification functions
    Add functions in passwords.py module to hash and verify user-entered
    password strings.

--- processes/passwords.py
+++ processes/passwords.py
@@ -1,6 +1,32 @@
-def hash_password():
-    return 'hash'
+import binascii
+import hashlib
+import os
 
 
-def verify_password():
-    return True
+# By CC attribution, the functions for hashing and verifying password are based on code at
+# https://www.vitoshacademy.com/hashing-passwords-in-python/
+def hash_pwd_str(provided_password: str) -> str:
+    """Returns a secure hash of the provided_password string to be stored."""
+    random_bytes = os.urandom(60)
+    salt = hashlib.sha256(random_bytes).hexdigest().encode('ascii')
+
+    provided_password = provided_password.encode('utf-8')
+    pwdhash = hashlib.pbkdf2_hmac('sha512', provided_password, salt, 100000)
+    pwdhash = binascii.hexlify(pwdhash)
+    return (salt + pwdhash).decode('ascii')
+
+
+def verify_pwd_str(provided_password: str, stored_hash: str) -> bool:
+    """Returns a boolean of whether provided_password matches the stored password/hash"""
+    salt = stored_hash[:64].encode('ascii')
+    stored_password = stored_hash[64:]
+    provided_password = provided_password.encode('utf-8')
+    pwdhash = hashlib.pbkdf2_hmac('sha512', provided_password, salt, 100000)
+    pwdhash = binascii.hexlify(pwdhash).decode('ascii')
+    return pwdhash == stored_password
+
+
+def enforce_strength(provided_password: str) -> bool:
+    """Returns a boolean of whether provided_password matches the program's strength requirements"""
+    # TODO: write function
+    return bool(provided_password)
--- tests/test_passwords.py
+++ tests/test_passwords.py
@@ -1,16 +1,23 @@
 from unittest import TestCase
+
 from processes import passwords
 
 
 class PasswordTest(TestCase):
+    test_pwd_list = ('generic password', 'uniɕode password', '', '12.345')
+
     def test_hash_password(self):
-        if passwords.hash_password():
-            pass
-        else:
-            self.fail()
+        for pwd_str in self.test_pwd_list:
+            hash_str = passwords.hash_pwd_str(pwd_str)
+            self.assertIsInstance(hash_str, str, 'Hash not a string')
+            self.assertEqual(len(hash_str), 192, 'Hash incorrect length')
 
     def test_verify_password(self):
-        if passwords.verify_password():
-            pass
-        else:
-            self.fail()
+        pwd_hash_list = map(passwords.hash_pwd_str, self.test_pwd_list)
+        for pwd_str, pwd_hash in zip(self.test_pwd_list, pwd_hash_list):
+            self.assertTrue(passwords.verify_pwd_str(pwd_str, pwd_hash),
+                            'Password verification failed on correct match')
+
+        for pwd_str, pwd_hash in zip(('test') * len(self.test_pwd_list), pwd_hash_list):
+            self.assertFalse(passwords.verify_pwd_str(pwd_str, pwd_hash),
+                             "Password verification should have failed but didn't")
