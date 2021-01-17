40a9ee93eb4c24d279549a9bff085ab706b36af8
tameTNT <30503695+tameTNT@users.noreply.github.com>
2021-01-17 16:42:02 +0000

    🎉Create new file for student interface tk code

---
+++ ui/student/__init__.py
@@ -0,0 +1,12 @@
+import logging
+import tkinter as tk
+import tkinter.messagebox as msg
+import tkinter.ttk as ttk
+from typing import Union, Type
+
+from data_tables import data_handling
+
+
+class StudentOverviewWindow:
+    def __init__(self, master: tk.Tk, db: data_handling.Database, padx=10, pady=5):
+        pass
