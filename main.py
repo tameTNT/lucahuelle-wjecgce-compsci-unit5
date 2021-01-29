a6e5ce86c093b3fceca081619e9b8ba0783d40d0
tameTNT <30503695+tameTNT@users.noreply.github.com>
2021-01-29 15:12:05 +0000

    🚚Restructure and generalise all GUI code
    Restructure tkinter GUI code to be more object oriented for greater
    simplicity in future implementation of new parts of the application and
     to fully take advantage of 'paged' window system.
    
    Begin GUI outline of StudentOverviewPage to be built upon with
    functionality in future commits.

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import logging
 import tkinter as tk
 from tkinter import messagebox
 
+import ui.landing
+import ui.student
 from data_tables import data_handling
-from ui.welcome import WelcomeLoginWindow
+from ui import RootWindow
 
 logging.basicConfig(filename='main_program.log',
                     filemode='w', level=logging.DEBUG,
@@ -13,14 +15,14 @@ logging.basicConfig(filename='main_program.log',
 def close_window_call(db_obj: data_handling.Database, tk_root: tk.Tk) -> None:
     """
     This function is activated by the WM_DELETE_WINDOW protocol (i.e. when the user presses X).
-    It makes the user confirm they want to close the program and saves program data.
+    It makes the user confirm they want to close the program and saves the program data to file.
 
     :param db_obj: the main Database object to which the current database state should be saved from
     :param tk_root: tkinter Tk() object which is main base window that user wanted to close
     """
     logging.debug('User pressed quit button')
-    if messagebox.askokcancel("Quit", "Are you sure you want to quit?\n"
-                                      "This will save all stored data."):
+    if messagebox.askokcancel("Quit",
+                              "Are you sure you want to quit?\nThis will save all stored data."):
         logging.debug('User chose to destroy window and exit program')
         # saves current database state from memory to file before closing
         db_obj.save_state_to_file()
@@ -33,7 +35,18 @@ if __name__ == '__main__':
     root = tk.Tk()
     main_database_obj = data_handling.Database()
     main_database_obj.load_state_from_file()  # loads last database state from file into memory
-    logging.debug('WelcomeLoginWindow initialised')
-    main_window = WelcomeLoginWindow(master=root, db=main_database_obj, padx=10, pady=2)
+
+    main_window = RootWindow(tk_root=root, db=main_database_obj)  # initialises tkinter root/base
+
+    PAGE_OBJ_LIST = (
+        ui.landing.Welcome,
+        ui.landing.StudentLogin,
+        ui.landing.StaffLogin,
+        ui.student.StudentAwardDashboard,
+    )  # TODO: a way to smartly collect all of these GenericPage classes?
+
+    # initialises actual tkinter window on Welcome page
+    main_window.initialise_window(page_obj_list=PAGE_OBJ_LIST, start_page=ui.landing.Welcome)
+    # binds above function to action of closing window - i.e. tkinter triggers func on close
     root.protocol("WM_DELETE_WINDOW", lambda: close_window_call(main_database_obj, root))
     root.mainloop()
--- ui/student/__init__.py
+++ ui/student/__init__.py
@@ -1,12 +1,45 @@
+from __future__ import annotations
+
 import logging
 import tkinter as tk
-import tkinter.messagebox as msg
 import tkinter.ttk as ttk
-from typing import Union, Type
 
+import ui
+import ui.landing
 from data_tables import data_handling
 
 
-class StudentOverviewWindow:
-    def __init__(self, master: tk.Tk, db: data_handling.Database, padx=10, pady=5):
-        pass
+class StudentAwardDashboard(ui.GenericPage):
+    page_name = 'Student Award Dashboard'
+
+    def __init__(self, pager_frame: ui.PagedMainFrame, student: data_handling.Student = None):
+        super().__init__(pager_frame=pager_frame)
+
+        # starts with a null value before being updated with student info below
+        self.student_name_welcome = tk.StringVar(value='Hi Null!')
+        self.welcome_text = ttk.Label(self, textvariable=self.student_name_welcome,
+                                      justify='center')
+        self.welcome_text.grid(row=0, column=0, columnspan=3, padx=self.padx, pady=self.pady)
+
+        self.logout_button = ttk.Button(self, text='Logout', command=self.logout)
+        self.logout_button.grid(row=1, column=0, padx=self.padx, pady=self.pady)
+
+        self.student = student  # stores all student information for the window
+        self.student_username = ''
+
+    def update_attributes(self, student: data_handling.Student, username: str) -> None:
+        # updates attributes with submitted parameters
+        self.student = student
+        self.student_username = username
+
+        # updates tkinter StringVars with new information received
+        self.student_name_welcome.set(f'Hi {self.student.fullname}!')
+
+    def logout(self):
+        """
+        Logs the student out of the page - returns them to Welcome page
+        """
+        logging.info(f'Username "{self.student_username}" '
+                     f'successfully logged out of student application')
+
+        self.pager_frame.change_to_page(ui.landing.Welcome)
