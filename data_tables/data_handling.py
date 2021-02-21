23babe9f321af8a839bdf71517d599fea45d9e80
tameTNT <30503695+tameTNT@users.noreply.github.com>
2021-02-21 21:06:17 +0000

    🐛Fix ID determining bug
    There was a logic error in the code that meant the correct id wasn't
    being automatically chosen.

--- data_tables/data_handling.py
+++ data_tables/data_handling.py
@@ -423,7 +423,9 @@ class SectionTable(Table):
         """
         taken_ids = {int(str_id) for str_id in self.row_dict.keys()}
         if taken_ids:
-            return set(range(max(taken_ids))).intersection(taken_ids).pop()
+            # range(1, max(taken_ids) + 2): a range of all possible ids between 1 and the max taken id+1
+            # set.difference finds any unused ids in this range and min() finds the smallest such id
+            return min(set(range(1, max(taken_ids) + 2)).difference(taken_ids))
         else:
             return 1
 
